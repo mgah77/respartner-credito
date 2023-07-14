a247adb1cb0477fb77d55a3453df4171f128165d
mgah77 <mauricio.gah@gmail.com>
2023-07-14 14:04:05 -0400

    add view

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,23 @@
     'name':'Addon Credito Clientes',
     'version':'1.0',
     'category':'General',
+    'summary': '',
+    'description': """
+    Addon
+
+    Credito a Clientes y Bloquedo
+
+
+       """,
+    'author' : 'M.Gah',
+    'website': '',
     'installable':'True',
     'auto_install':'False',
     'application':'True'
+    'depends': ['base','sale','account'],
+    'data': [
+            'security/groups.xml',
+            'security/ir.model.access.csv',
+            'views/add_credit.xml'           
+    ],
 }
---
+++ models/add_credit.py
@@ -0,0 +1,6 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class Add_credit(models.Model):
+    _inherit = 'res.partner'
