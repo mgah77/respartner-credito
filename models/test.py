5cb60edd01510b6522d3c4995a61f41b90d84c1c
mgah77 <mauricio.gah@gmail.com>
2023-07-25 15:52:34 -0400

    Update test.py

--- models/test.py
+++ models/test.py
@@ -6,4 +6,4 @@ class Test1(models.Model):
     _inherit = ['account.invoice']
 
     credito = fields.Float("Credito")
-    estadodeuda = fields.Char(string="Estado")
+    estadodeuda = fields.Char(string="Estado", readonly = True)
