b5871d1d183c0e8889d54755258be40396d755df
mgah77 <mauricio.gah@gmail.com>
2023-07-31 21:00:37 -0400

    55

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     'installable': True,
     'auto_install': False,
     'application': True,
-    'depends': ['base','sale','account'],
+    'depends': ['base','sale','account','l10n_cl_fe'],
     'data': [
             'security/groups.xml',
             'security/ir.model.access.csv',
