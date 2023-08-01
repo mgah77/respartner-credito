cfd4a4da9c5b955d3570a45e1cfa51aa9f7dbb83
mgah77 <mauricio.gah@gmail.com>
2023-07-31 21:18:30 -0400

    Update test.py

--- models/test.py
+++ models/test.py
@@ -4,11 +4,11 @@ from odoo import models, fields, api, _
 class Test1(models.Model):
     _inherit = ['account.invoice']
 
-    credito = fields.Float(string="Credito", compute="_compute_cantidad_vencida")
-    estadodeuda = fields.Char(string="Estado", readonly = True)
+    #credito = fields.Float(string="Credito")
+    #estadodeuda = fields.Char(string="Estado", readonly = True)
 
-    @api.depends('partner_id')
-    def _compute_cantidad_vencida(self):
-        for record in self:
-            record.credito = 0
-        return
+    #@api.depends('partner_id')
+    #def _compute_cantidad_vencida(self):
+    #    for record in self:
+    #        record.credito = 0
+    #    return
