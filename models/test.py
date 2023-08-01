2e1ad5cc90fcadb340f313eef8468e5350a3fe20
mgah77 <mauricio.gah@gmail.com>
2023-07-31 21:04:30 -0400

    65

--- models/test.py
+++ models/test.py
@@ -5,5 +5,17 @@ from odoo.exceptions import ValidationError
 class Test1(models.Model):
     _inherit = ['account.invoice']
 
-    credito = fields.Float("Credito")
+    credito = fields.Float(string="Credito", compute="_compute_cantidad_vencida")
     estadodeuda = fields.Char(string="Estado", readonly = True)
+
+    @api.depends('partner_id')
+    def _compute_cantidad_vencida(self):
+        Invoice = self.env['account.invoice']
+        for record in self:
+            if record.cliente:
+                vencido = Invoice.search([('partner_id', '=', record.partner_id),('type', '=', 'out_invoice'),('state', '=', 'open')])
+                total_vencido = sum(factura.amount_total for factura in vencido)
+                record.credito = total_vencido
+            else:
+                record.credito = 0
+        return
