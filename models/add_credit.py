79e7d795ef6c342f1e33cd1425ebea062397e3d8
mgah77 <mauricio.gah@gmail.com>
2023-08-03 19:05:52 -0400

    Update add_credit.py

--- models/add_credit.py
+++ models/add_credit.py
@@ -5,16 +5,16 @@ from odoo.exceptions import ValidationError
 class Add_credit(models.Model):
     _inherit = ['account.invoice']
 
-    credito = fields.Float(string="Credito",compute='_compute_cantidad_vencida')
+    credito = fields.Float(string="Credito")
 
-    @api.depends('partner_id')
-    def _compute_cantidad_vencida(self):
-        Invoice = self.env['account.invoice']
-        for record in self:
-            if record.partner_id:
-                pre_total = Invoice.search([('partner_id', '=', record.partner_id),('type', '=', 'out_invoice'),('state', '=', 'open')])
-                total = sum(factura.amount_total for factura in pre_total)
-                record.credito = total
-            else
-                record.credito = 0
-        return
+    #@api.depends('partner_id')
+    #def _compute_cantidad_vencida(self):
+    #    Invoice = self.env['account.invoice']
+    #    for record in self:
+    #        if record.partner_id:
+    #            pre_total = Invoice.search([('partner_id', '=', record.partner_id),('type', '=', 'out_invoice'),('state', '=', 'open')])
+    #            total = sum(factura.amount_total for factura in pre_total)
+    #            record.credito = total
+    #        else
+    #            record.credito = 0
+    #    return
