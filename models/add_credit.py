e4f3b5e3308e9cf9442368affc0a5acbc425594e
mgah77 <mauricio.gah@gmail.com>
2023-08-03 19:59:15 -0400

    Update add_credit.py

--- models/add_credit.py
+++ models/add_credit.py
@@ -6,14 +6,14 @@ class Add_credit(models.Model):
     _inherit = ['account.invoice']
 
     credito = fields.Float(string="Credito", compute='_compute_cantidad_vencida')
-    estado = fields.Selection([('active','Activo'),('deudor','Moroso'),('tope','Excede Credito')], string="Estado", default='active')
+    estado = fields.Selection([('active','Activo'),('deudor','Moroso'),('tope','Excede Credito')], string="Estado", default='active', readonly=True)
 
     @api.depends('partner_id')
     def _compute_cantidad_vencida(self):
         Invoice = self.env['account.invoice']
         for record in self:
             if record.partner_id:
-                pre_total = Invoice.search([('partner_id', '=', record.partner_id.id),('type', '=', 'out_invoice'),('state', '=', 'open')])
+                pre_total = Invoice.search([('partner_id', '=', record.partner_id.id),('type', '=', 'out_invoice'),('state', '=', 'open'),('date_due', '>', fields.Date.today())])
                 total = sum(factura.amount_total for factura in pre_total)
                 record.credito = total
             else:
