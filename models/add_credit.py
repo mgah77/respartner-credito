28a24dd97f3868e8bf61b3463ef1df5f390dec46
mgah77 <mauricio.gah@gmail.com>
2023-08-03 19:31:53 -0400

    ryt

--- models/add_credit.py
+++ models/add_credit.py
@@ -5,17 +5,17 @@ from odoo.exceptions import ValidationError
 class Add_credit(models.Model):
     _inherit = ['account.invoice']
 
-    credito = fields.Float(string="Credito")
+    credito = fields.Float(string="Credito", compute='_compute_cantidad_vencida')
     estado = fields.Selection([('active','Activo'),('deudor','Moroso'),('tope','Excede Credito')], string="Estado", default='active')
 
-    #@api.depends('partner_id')
-    #def _compute_cantidad_vencida(self):
-    #    Invoice = self.env['account.invoice']
-    #    for record in self:
-    #        if record.partner_id:
-    #            pre_total = Invoice.search([('partner_id', '=', record.partner_id),('type', '=', 'out_invoice'),('state', '=', 'open')])
-    #            total = sum(factura.amount_total for factura in pre_total)
-    #            record.credito = total
-    #        else
-    #            record.credito = 0
-    #    return
+    @api.depends('partner_id')
+    def _compute_cantidad_vencida(self):
+        Invoice = self.env['account.invoice']
+        for record in self:
+            if record.partner_id:
+                pre_total = Invoice.search([('partner_id', '=', record.partner_id),('type', '=', 'out_invoice'),('state', '=', 'open')])
+                total = sum(factura.amount_total for factura in pre_total)
+                record.credito = total
+            else
+                record.credito = 0
+        return
