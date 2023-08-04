21a12772146caadf3394818064627a0860c0f70e
mgah77 <mauricio.gah@gmail.com>
2023-08-04 09:28:11 -0400

    jbvcx

--- models/add_credit.py
+++ models/add_credit.py
@@ -25,3 +25,27 @@ class Add_credit(models.Model):
             else:
                 record.credito = 0
         return
+
+class Add_credit_venta(models.Model):
+    _inherit = ['sale']
+
+    credito = fields.Float(string="Credito", compute='_compute_cantidad_vencida')
+    estado = fields.Selection([('active','Activo'),('deudor','Cliente Moroso'),('tope','Excede Credito')], string="Estado", default='active', readonly=True)
+
+    @api.depends('partner_id')
+    def _compute_cantidad_vencida(self):
+        Invoice = self.env['account.invoice']
+        for record in self:
+            if record.partner_id:
+                pre_total = Invoice.search_count([('partner_id', '=', record.partner_id.id),('type', '=', 'out_invoice'),('state', '=', 'open'),('date_due', '<', fields.Date.today())])
+                #total = sum(factura.amount_total for factura in pre_total)
+                if (pre_total > 0):
+                    record.estado = 'deudor'
+                    record.state = 'cancel'
+                else:
+                    record.estado = 'active'  
+                    record.state = 'draft'  
+                record.credito = pre_total
+            else:
+                record.credito = 0
+        return
